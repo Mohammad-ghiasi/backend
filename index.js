dccd12235ae59baf5b3649bacfd3b36d4e81b9af
mohammad <mohammadghiasi005@gmail.com>
2024-07-26 15:18:58 +0330

    CROS

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+const corsOptions = {
+  origin: '*', // Allow all origins
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allow these methods
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
 // Use CORS middleware
-app.use(cors());
 
 // Define the /hello route
 app.get('/hello', (req, res) => {
